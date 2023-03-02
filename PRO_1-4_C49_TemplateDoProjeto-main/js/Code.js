c7c65c247c21b4082097c5f4cdf3a8eecc2209ea
Gugu2009 <114031352+Gugu2009@users.noreply.github.com>
2023-03-02 20:00:50 -0300

    Add files via upload

---
+++ PRO_1-4_C49_TemplateDoProjeto-main/js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+    
+   
+    textSize(15)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!", 100,70)
+
+   
+    textSize(15)
+    fill("lightblue")
+    text("Dica: É um conjunto de comandos que executam uma tarefa expecifica", 500,170)
+
+   
+    textSize(15)
+    fill("lightblue")
+    text("Dica: Pode armazenar todas as informações mais valiosas do mundo!", 100,270)
+
+}
